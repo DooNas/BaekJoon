60c865ba2d684263c04ae898b093278e638ffbbf
도나스 <jaejoon4929@gmail.com>
2022-05-13 18:38:38 +0900

    [Bronze IV] Title: 주사위 세개, Time: 208 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+        
+        int a = input.nextInt();
+       	int b = input.nextInt();
+        int c = input.nextInt();
+
+        int sum = 0;
+
+        if ( a == b && b == c) sum = 10000 + a * 1000;
+        else if ( a == b || b == c || a == c){
+            if( a == b ) sum = 1000 + a * 100;
+            else if( a == c) sum = 1000 + c * 100;
+            else if( b == c) sum = 1000 + b * 100;
+        }else{
+            if(a < b){
+                if(b < c) sum = c * 100;
+                else sum = b * 100;
+            }else{
+                if(a < c) sum = c * 100;
+                else sum = a * 100;
+            }
+        }
+        System.out.print(sum);
+        input.close();
+    }
+}
