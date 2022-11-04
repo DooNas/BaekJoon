8944d1dba04ab0e265ef595e3de5a4a6e19e1fc7
도나스 <jaejoon4929@gmail.com>
2022-11-04 14:38:42 +0900

    [Bronze V] Title: A+B, Time: 64 ms, Memory: 5488 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace baekjoon
+{
+    class no1000
+    {
+        static void Main(string[] args)
+        {
+            string[] a = Console.ReadLine().Split(); //입력받은 값을 공백 기준으로 배열로 나눈다.
+            Console.WriteLine(int.Parse(a[0]) + int.Parse(a[1]));
+        }
+    }
+}
