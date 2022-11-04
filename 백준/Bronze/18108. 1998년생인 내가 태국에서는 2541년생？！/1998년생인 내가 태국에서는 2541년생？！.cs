85c4009bcf240d793c583d057c9e7bcac487b671
도나스 <jaejoon4929@gmail.com>
2022-11-04 15:31:56 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 64 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace baekjoon
+{
+    class no18108
+    {
+        static void Main(string[] args)
+        {
+            int year = int.Parse(Console.ReadLine());
+            Console.WriteLine(year - 543);
+        }
+    }
+}
