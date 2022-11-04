5a2f03d55894ed53503ef9ba26837dc8f9998c58
도나스 <jaejoon4929@gmail.com>
2022-11-04 15:25:22 +0900

    [Bronze V] Title: ??!, Time: 64 ms, Memory: 5368 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace baekjoon
+{
+    class no10926
+    {
+        static void Main(string[] args){
+            string name = Console.ReadLine();
+            Console.WriteLine(name + "??!");
+        }
+    }
+}
