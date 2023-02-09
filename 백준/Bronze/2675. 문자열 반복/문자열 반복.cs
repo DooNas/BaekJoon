ab7a62c0a9f3eb68e7bdc36340780901be1bc3e9
도나스 <jaejoon4929@gmail.com>
2023-02-09 14:06:47 +0900

    [Bronze II] Title: 문자열 반복, Time: 72 ms, Memory: 5652 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.cs
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.cs
@@ -4,22 +4,18 @@ class Program
     {
         List<string> list = new List<string>();
 
-        string Count = Console.ReadLine();
-        for(int i = 0; i < int.Parse(Count); i++)
+        int Count = int.Parse(Console.ReadLine());
+        for(int i = 0; i < Count; i++)
         {
             list.Add(Console.ReadLine());
         }
-        //원하는 만큼 진행 N string
 
         foreach(string str in list)
         {
             int count = int.Parse(str[0].ToString());
             for(int i = 2; i < str.Length; i++)
             {
-                for(int j = 0; j < count; j++)
-                {
-                    Console.Write(str[i]);
-                }
+                Console.Write(new string(str[i], count));
             }
             Console.WriteLine();
         }
