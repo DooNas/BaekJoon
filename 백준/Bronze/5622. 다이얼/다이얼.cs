084294a1038cd1fe9de6424348789bebc8f7927b
도나스 <jaejoon4929@gmail.com>
2023-02-09 15:03:58 +0900

    [Bronze II] Title: 다이얼, Time: 64 ms, Memory: 5560 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.cs
+++ 백준/Bronze/5622. 다이얼/다이얼.cs
@@ -3,14 +3,13 @@ class Program
     public static void Main()
     {
         List<string> alphabat = new List<string>{"ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"};
-        List<int> time = new List<int>{3,4,5,6,7,8,9,10};
 
         string input = Console.ReadLine();
         int sum = 0;
         foreach(char c in input){
             for(int i = 0; i < alphabat.Count; i++){
                 if(alphabat[i].Contains(c)){
-                    sum += time[i];
+                    sum += i+3;
                 }
             }
         }
